bec146ca9a522edf27294a7e32f2320de67edcbb
羽波 <31097812+SonodaHanami@users.noreply.github.com>
2024-06-19 08:04:28 +0800

    修正拼音

--- validation_pypinyin.py
+++ validation_pypinyin.py
@@ -1,13 +1,14 @@
 import json
 from pypinyin import pinyin, Style
 
-answers = json.load(open('answers_arknights.json', 'r'))
+answer_dict = json.load(open('answers_arknights.json', 'r'))
+pinyin_dict = json.load(open('pinyin_arknights.json', 'r'))
 
 count = 0
-for answer in answers:
+for answer in answer_dict:
     pys = [py[0] for py in pinyin(answer['word'], style=Style.TONE3, v_to_u=True)]
-    if pys != answer['pinyin']:
-        print(answer['word'], answer['pinyin'], pys)
+    if pys != pinyin_dict[answer['word']]:
+        print(answer['word'], pinyin_dict[answer['word']], pys)
         count += 1
 
-print('unmatched/total = {}/{}({:.2f}%)'.format(count, len(answers), 100 * count / len(answers)))
+print('unmatched/total = {}/{}({:.2f}%)'.format(count, len(answer_dict), 100 * count / len(answer_dict)))
